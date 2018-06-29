a355e0db3895e130703bf051ef038bef71490bb4
dandan <771773847@qq.com>
2018-06-29 10:20:21 +0800

    Site updated: 2018-06-29 10:20:20

--- app/hostloc/js/common.js
+++ app/hostloc/js/common.js
@@ -1,5 +1,5 @@
 if (window.location.protocol.split(":")[0] === "https") {
-//    window.location.replace("http://" + window.location.href.split("//")[1]);
+    window.location.replace("http://" + window.location.href.split("//")[1]);
 }
 function reverseString(str) {
     return str.split("").reverse().join("");
